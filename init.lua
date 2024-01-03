3fbbdc9d876903003a4a5dcbcc8b66a9e678a161
Alexandre Vernotte <alexandre.vernotte@femto-st.fr>
2024-01-03 08:02:40 +0100

    probably something

--- init.lua
+++ init.lua
@@ -97,6 +97,7 @@ require('lazy').setup({
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
+      'neovim/nvim-lspconfig',
       -- Snippet Engine & its associated nvim-cmp source
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
@@ -104,6 +105,8 @@ require('lazy').setup({
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-cmdline',
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
